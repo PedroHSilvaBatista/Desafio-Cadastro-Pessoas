d1d0be66d9487b00debe54e4eb5985d12aa03603
Pedro <pedrohenriquesb02@gmail.com>
2026-05-31 00:15:31 -0300

    Criando a entidade Pessoa

--- src/main/java/br/com/pessoas/cadastro/desafio/model/Pessoa.java
+++ src/main/java/br/com/pessoas/cadastro/desafio/model/Pessoa.java
@@ -1,4 +1,33 @@
 package br.com.pessoas.cadastro.desafio.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "pessoas")
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeCompleto;
+    private String documentoCpf;
+    private String email;
+    private LocalDate dataNascimento;
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+
+    @Enumerated(EnumType.STRING)
+    private Estado estado;
+
+    @Column(length = 7)
+    private String login;
+
+    public Pessoa() {
+
+    }
 }
